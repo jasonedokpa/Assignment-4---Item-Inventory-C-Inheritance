b29489dc0e05a68ae4113d5a3b5586e6bf0efbcf
Jason Edokpa <jasonedokpa@gmail.com>
2022-04-01 21:25:35 -0500

    converted indentation to tabs

--- Armour.cpp
+++ Armour.cpp
@@ -2,45 +2,45 @@
 
 //------------------------------------------------------------------------------
 Armour::Armour()
-    :Item("Armour", false), // There should be something simliar in Consumable.cpp
-     material(),
-     modifier(),
-     element()
+	:Item("Armour", false), // There should be something simliar in Consumable.cpp
+	 material(),
+	 modifier(),
+	 element()
 {
-    this->durability    = 0;
-    this->defense       = 0;
-    this->modifierLevel = 1;
+	this->durability    = 0;
+	this->defense       = 0;
+	this->modifierLevel = 1;
 }
 
 //------------------------------------------------------------------------------
 void Armour::display(std::ostream& outs) const
 {
-    // Implement this function
-    outs << "  Nme: " << name       << std::endl;
-    outs << "  Dur: " << durability << std::endl;
-    outs << "  Def: " << defense    << std::endl;
-    outs << "  Mtl: " << material   << std::endl;
-    outs << "  Mdr: " << modifier   << " (Lvl " << modifierLevel << ")" << std::endl;
-    outs << "  Emt: " << element    << std::endl;
+	// Implement this function
+	outs << "  Nme: " << name       << std::endl;
+	outs << "  Dur: " << durability << std::endl;
+	outs << "  Def: " << defense    << std::endl;
+	outs << "  Mtl: " << material   << std::endl;
+	outs << "  Mdr: " << modifier   << " (Lvl " << modifierLevel << ")" << std::endl;
+	outs << "  Emt: " << element    << std::endl;
 }
 
 //------------------------------------------------------------------------------
 void Armour::read(std::istream& ins)
 {
-    // Implement this function
-    ins >> this->name;
-    ins >> this->material;
-    ins >> this->durability;
-    ins >> this->defense;
-    ins >> this->modifier;
-    ins >> this->modifierLevel;
-    ins >> this->element;
+	// Implement this function
+	ins >> this->name;
+	ins >> this->material;
+	ins >> this->durability;
+	ins >> this->defense;
+	ins >> this->modifier;
+	ins >> this->modifierLevel;
+	ins >> this->element;
 }
 
 //------------------------------------------------------------------------------
 Item* Armour::clone() const
 {
-    // Implement this function
-    return new Armour(*this);
+	// Implement this function
+	return new Armour(*this);
 }
 
--- Consumable.cpp
+++ Consumable.cpp
@@ -5,25 +5,25 @@ Consumable::Consumable()
 :Item("Consumable", true),
 effect()
 {
-    uses = 0;
+	uses = 0;
 }
 
 //------------------------------------------------------------------------------
 // Add the definition for this function
 void Consumable::display(std::ostream& outs) const
 {
-    outs << "  Nme: " << name   << std::endl;
-    outs << "  Eft: " << effect << std::endl;
-    outs << "  Use: " << uses   << std::endl;
+	outs << "  Nme: " << name   << std::endl;
+	outs << "  Eft: " << effect << std::endl;
+	outs << "  Use: " << uses   << std::endl;
 }
 
 //------------------------------------------------------------------------------
 // Add the definition for this function
 void Consumable::read(std::istream& ins)
 {
-    ins >> this->name;
-    ins >> this->effect;
-    ins >> this->uses;
+	ins >> this->name;
+	ins >> this->effect;
+	ins >> this->uses;
 }
 
 
@@ -31,5 +31,5 @@ void Consumable::read(std::istream& ins)
 // Add the definition for this function
 Item* Consumable::clone() const
 {
-    return new Consumable(*this);
+	return new Consumable(*this);
 }
