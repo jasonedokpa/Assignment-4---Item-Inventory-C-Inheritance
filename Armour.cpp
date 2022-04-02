ec4ced8f0c451b67cd4c0c910161563f2e550b0f
Jason Edokpa <jasonedokpa@gmail.com>
2022-04-01 21:24:23 -0500

    completed assignment

--- Armour.cpp
+++ Armour.cpp
@@ -4,7 +4,8 @@
 Armour::Armour()
     :Item("Armour", false), // There should be something simliar in Consumable.cpp
      material(),
-     modifier()
+     modifier(),
+     element()
 {
     this->durability    = 0;
     this->defense       = 0;
@@ -15,18 +16,31 @@ Armour::Armour()
 void Armour::display(std::ostream& outs) const
 {
     // Implement this function
+    outs << "  Nme: " << name       << std::endl;
+    outs << "  Dur: " << durability << std::endl;
+    outs << "  Def: " << defense    << std::endl;
+    outs << "  Mtl: " << material   << std::endl;
+    outs << "  Mdr: " << modifier   << " (Lvl " << modifierLevel << ")" << std::endl;
+    outs << "  Emt: " << element    << std::endl;
 }
 
 //------------------------------------------------------------------------------
 void Armour::read(std::istream& ins)
 {
     // Implement this function
+    ins >> this->name;
+    ins >> this->material;
+    ins >> this->durability;
+    ins >> this->defense;
+    ins >> this->modifier;
+    ins >> this->modifierLevel;
+    ins >> this->element;
 }
 
 //------------------------------------------------------------------------------
 Item* Armour::clone() const
 {
     // Implement this function
-    return nullptr; // remove this line
+    return new Armour(*this);
 }
 
--- Consumable.cpp
+++ Consumable.cpp
@@ -2,17 +2,34 @@
 
 //------------------------------------------------------------------------------
 Consumable::Consumable()
+:Item("Consumable", true),
+effect()
 {
+    uses = 0;
 }
 
 //------------------------------------------------------------------------------
-//void Consumable::display(std::ostream& outs) const
 // Add the definition for this function
+void Consumable::display(std::ostream& outs) const
+{
+    outs << "  Nme: " << name   << std::endl;
+    outs << "  Eft: " << effect << std::endl;
+    outs << "  Use: " << uses   << std::endl;
+}
 
 //------------------------------------------------------------------------------
-// void Consumable::read(std::istream& ins)
 // Add the definition for this function
+void Consumable::read(std::istream& ins)
+{
+    ins >> this->name;
+    ins >> this->effect;
+    ins >> this->uses;
+}
+
 
 //------------------------------------------------------------------------------
-// Item* Consumable::clone() const
 // Add the definition for this function
+Item* Consumable::clone() const
+{
+    return new Consumable(*this);
+}
